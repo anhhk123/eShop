0b599ca88d07360094a72b24a920fc3d1d659939
anhhk123 <anhcao244@gmail.com>
2024-06-21 14:26:50 +0700

    Add Configuration

---
+++ README.md
@@ -0,0 +1,8 @@
+# ASP.NET Core 6.0 project from Cao Anh
+## Technologies
+- Asp.net Core 6.0
+-Entity FrameWork Core 6.0
+## Install Packages
+- Microsoft.EntityFrameWorkCore.Tools
+- Microsoft.EntityFrameWorkCore.Design
+- Microsoft.EntityFrameWorkCore.SqlServer
---
+++ eShop.Data/Configgurations/ProductInCateogoryConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using eShop.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.Data.Configgurations
+{
+    public class ProductInCateogoryConfiguration : IEntityTypeConfiguration<ProductInCategory>
+    {
+        public void Configure(EntityTypeBuilder<ProductInCategory> builder)
+        {
+            builder.ToTable("ProductInCateogories");
+            builder.HasKey(t =>  new {t.ProductId, t.CategoryId});
+            builder.HasOne(t => t.Product).WithMany(pc => pc.ProductInCategories).HasForeignKey(t => t.ProductId);
+
+
+            builder.HasOne(c => c.Category).WithMany(pc => pc.ProductInCategories).HasForeignKey(t => t.CategoryId);
+        }
+    }
+}
