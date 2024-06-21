d55d66e7aee0c4fa6addfaa7faec6d54d6dd41cd
anhhk123 <anhcao244@gmail.com>
2024-06-21 14:55:41 +0700

    Up full configuration

---
+++ eShop.Data/Configgurations/ContactConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using eShop.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.Data.Configgurations
+{
+    public class ContactConfiguration : IEntityTypeConfiguration<Contact>
+    {
+        public void Configure(EntityTypeBuilder<Contact> builder)
+        {
+            builder.ToTable("Contacts");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.Name).HasMaxLength(200).IsRequired();
+            builder.Property(x => x.PhoneNumber).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.Messsage).IsRequired();
+
+        }
+    }
+}
